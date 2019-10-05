279dc25400fa08cf3d2b98534b18cbce01c537a3
Krone Corylus <ahustinkrone@gmail.com>
2019-10-05 11:40:49 -0300

    fix rm onboarding dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OnboardingComponent } from './dialogs/onboarding/onboarding.component';
 import { OnboardingHeaderComponent } from './components/onboarding-header/onboarding-header.component';
 import { OnboardingContainerComponent } from './components/onboarding-container/onboarding-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OnboardingComponent,
     OnboardingHeaderComponent,
     OnboardingContainerComponent
   ],
