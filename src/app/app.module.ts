ff81080881799eca27c9edcc29bcc66893abbd01
Krone Corylus <ahustinkrone@gmail.com>
2019-10-05 11:36:09 -0300

    Creo componentes para onboarding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OnboardingComponent } from './dialogs/onboarding/onboarding.component';
+import { OnboardingHeaderComponent } from './components/onboarding-header/onboarding-header.component';
+import { OnboardingContainerComponent } from './components/onboarding-container/onboarding-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OnboardingComponent,
+    OnboardingHeaderComponent,
+    OnboardingContainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/onboarding-container/onboarding-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-onboarding-container',
+  templateUrl: './onboarding-container.component.html',
+  styleUrls: ['./onboarding-container.component.css']
+})
+export class OnboardingContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
