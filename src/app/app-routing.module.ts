44f19cd71189cc398056caba26b3ad602c0c9f8f
jeremias98 <ramirezjeremias98@gmail.com>
2019-10-05 12:23:52 -0300

    Home con estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { OnboardingComponent } from './components/onboarding/onboarding.componen
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home.component';
 
 
 const routes: Routes = [
-  {path : '/',  component : OnboardingComponent}
+  {path : '',  component : HomeComponent}
 ];
 
 @NgModule({
